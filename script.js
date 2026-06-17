cad35ca743e7681cdcc1144ccb4b49527cb90108
10662856 <liyihao@boe.com.cn>
2026-06-17 13:23:09 +0800

    fix: even more turn server

--- script.js
+++ script.js
@@ -14,13 +14,20 @@ const peer = new Peer(undefined, {
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
-            { urls: 'stun:stun3.l.google.com:19302' },
-            { urls: 'stun:stun4.l.google.com:19302' },
-            // 备用公共 TURN (稳定性有限，建议自建或购买)
             {
-                urls: 'turn:numb.viagenie.fr',
-                username: 'webrtc@live.com',
-                credential: 'webrtc'
+                urls: 'turn:openrelay.metered.ca:80',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            },
+            {
+                urls: 'turn:openrelay.metered.ca:443',
+                username: 'openrelayproject',
+                credential: 'openrelayproject'
+            },
+            {
+                urls: 'turn:global.turn.twilio.com:3478?transport=tcp',
+                username: 'test',
+                credential: 'test'
             }
         ]
     }
